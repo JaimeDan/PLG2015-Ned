bb77291fdcd05faf994db6a439a9be8ce81d075c
Aryalexa <Aryalexa@users.noreply.github.com>
2015-05-11 13:53:35 +0200

    tiny actualizado por el NOT

--- sintaxis/run2.bat
+++ sintaxis/run2.bat
@@ -1,2 +1,2 @@
-Java -cp cup.jar java_cup.Main -parser AnalizadorSintacticoTiny -symbols ClaseLexica -nopositions Tiny.cup
+Java -cp cup.jar java_cup.Main -parser AnalizadorSintacticoTiny -symbols ClaseLexica -nopositions Tiny-mejor.cup
 pause
